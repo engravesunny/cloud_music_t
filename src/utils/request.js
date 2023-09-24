3d9dadbacae604b446be8da234cf094313461017
kecat <hutao-keqing@outlook.com>
2023-09-24 16:21:43 +0800

    api

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 
 const baseURL = import.meta.env.VITE_SRC
 const request = axios.create({
-  baseURL: '/api',
+  // baseURL: '/api',
   // baseURL:'http://localhost:3000/',
   // baseURL:'http://47.93.87.206:3000/',
-  // baseURL: "http://kecat.top:3000",
+  baseURL: "http://kecat.top:3000",
   withCredentials: true,
   timeout: 10000
 })
